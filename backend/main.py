0aba4f100dbc1a3d4a1c0af77c59b20f2f6ee79c
Tharuni79 <tharunireddyjonnalagadda@gmail.com>
2026-03-01 15:14:54 +0530

    Added WebSocket signaling support

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
+
+# Store active connections by room
+connections = {}
 
 @app.get("/")
 def home():
     return {"message": "Video Conference Backend Running"}
+
+@app.websocket("/ws/{room_id}")
+async def websocket_endpoint(websocket: WebSocket, room_id: str):
+    await websocket.accept()
+
+    if room_id not in connections:
+        connections[room_id] = []
+
+    connections[room_id].append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            # Send message to other users in same room
+            for connection in connections[room_id]:
+                if connection != websocket:
+                    await connection.send_text(data)
+
+    except WebSocketDisconnect:
+        connections[room_id].remove(websocket)
